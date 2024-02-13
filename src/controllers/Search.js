e45b39ba0956f264816a3dabc430c1a148d7ab97
sarahlte <sarah_leconte@msn.com>
2024-02-13 11:36:00 +0100

    add

--- src/controllers/Search.js
+++ src/controllers/Search.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+/* import axios from 'axios'; */
 
 import viewNav from '../views/nav';
 import viewUser from '../views/user';
-import viewChat from '../views/chat';
-import Bot from '../controllers/contact';
+/* import viewChat from '../views/chat'; */
+import Bot from './contact';
 import viewContacts from '../views/contacts';
 
 const Search = class {
@@ -11,7 +11,7 @@ const Search = class {
     this.el = document.querySelector('#root');
     this.params = params;
     this.data = [];
-    
+
     this.run();
   }
 
@@ -58,7 +58,7 @@ const Search = class {
   }
 
   run() {
-    /*const { results } = this.params;*/
+    /* const { results } = this.params; */
     this.data = Bot();
 
     /* axios
--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -3,8 +3,8 @@ export default () => {
     this.nom = nom;
     this.image = image;
     this.statut = statut;
-  };
-  let bot1 = new Bot('Wall-e', 'https://static.wikia.nocookie.net/heros/images/6/61/EVE_.webp/revision/latest?cb=20230801175117&path-prefix=fr', 'online');
-  let bot = {bot : (bot1)};
+  }
+  const bot1 = new Bot('Wall-e', 'https://static.wikia.nocookie.net/heros/images/6/61/EVE_.webp/revision/latest?cb=20230801175117&path-prefix=fr', 'online');
+  const bot = { bot: (bot1) };
   return bot;
-}
+};
