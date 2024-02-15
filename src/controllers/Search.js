a447fb308377aff0ed95076ba913c9b731139569
sarahlte <sarah_leconte@msn.com>
2024-02-15 16:35:31 +0100

    add

--- src/controllers/Search.js
+++ src/controllers/Search.js
@@ -11,7 +11,7 @@ const Search = class {
     this.params = params;
     this.data = [];
     if (!sessionStorage.getItem('bot')) {
-      sessionStorage.setItem('bot', 1);
+      sessionStorage.setItem('bot', 0);
     }
 
     this.run();
