7ed50cc97257f99c9380ba9ef4a93b42022ec79f
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-02-16 14:17:59 +0100

    add

--- src/views/chatUser.js
+++ src/views/chatUser.js
@@ -1,7 +1,10 @@
 export default (message) => (`
 <div class="bubble-display-user">
     <div class="bubble-sent user">
-        <div class="bubble-pseudo">Pseudo utilisateur</div>
+        <div class="pseudo">
+            <div class="date">00.00.00 à 00:13</div>
+            <div class="bubble-pseudo">Pseudo utilisateur</div>
+        </div>
         <span class="bubble-bubble-user"> ${message} </span>
     </div>
     <img src="https://www.macplus.net/app/uploads/2008/05/jpg_wall_e_eve.jpg" class="list-avatar"/>
